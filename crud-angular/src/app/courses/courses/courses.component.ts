af0690103fa7295b8468bf94aec2c9fd4e1dafc7
VitorSilva11 <svitoryago@hotmail.com>
2022-08-16 09:49:17 -0300

    adding httpclient in service

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 
@@ -9,12 +10,12 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
 
   displayedColumns = ['name', 'category'];
 
   constructor(private serviceCourse: CoursesService) {
-    this.courses = serviceCourse.findAll();
+    this.courses = this.serviceCourse.findAll();
   }
 
   ngOnInit(): void {
